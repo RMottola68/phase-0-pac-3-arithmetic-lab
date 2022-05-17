1331cb18d652bd6ca7a4fc84ff87d49dcbe1af74
Ryan Mottola <rmottola68@gmail.com>
2022-05-16 20:02:48 -0400

    commit tree d097b80f94e3b8303d9f66202a8423ee5eccc773

---
+++ index.js
@@ -0,0 +1,33 @@
+let add = (a, b) => {
+    return a+b;
+}
+
+let subtract = (a, b) => {
+    return a-b;
+}
+
+let multiply = (a, b) => {
+    return a*b;
+}
+
+let divide = (a, b) => {
+    return a/b;
+}
+
+let increment = (n) => {
+    n++;
+    return n;
+}
+
+let decrement = (n) => {
+    n--
+    return n;
+}
+
+let makeInt = (n) => {
+    return parseInt(n, 10);
+}
+
+let preserveDecimal = (n) => {
+    return parseFloat(n);
+}
